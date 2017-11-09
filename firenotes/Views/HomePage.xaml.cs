dccaf5dbeed77aa294fdb81a75fcbb1b9da0a442
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-09 13:49:39 +0100

    #47 | add the note details page

--- firenotes/Views/HomePage.xaml.cs
+++ firenotes/Views/HomePage.xaml.cs
@@ -1,14 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace firenotes.Views
 {
     public partial class HomePage : ContentPage
     {
+        ObservableCollection<string> Items = new ObservableCollection<string>();
+
         public HomePage()
         {
+            Items.Add("Number One");
+            Items.Add("Number One");
+            Items.Add("Number One");
+            Items.Add("Number One");
+            Items.Add("Number One");
+            Items.Add("Number One");
+            Items.Add("Number One");
+
             InitializeComponent();
 
             this.Title = "Your Notes";
@@ -17,6 +26,11 @@ namespace firenotes.Views
         protected async void AddNote(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new NewNotePage());
+        }
+
+        async void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new NoteDetailsPage());
         }
     }
 }
---
+++ firenotes/Views/NoteDetailsPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace firenotes.Views
+{
+    public class NoteDetailsPage : ContentPage
+    {
+        public NoteDetailsPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
