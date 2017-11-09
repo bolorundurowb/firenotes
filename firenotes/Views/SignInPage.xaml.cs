789bbf5d61e90ede5002b086dcd07dcafef42566
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-09 14:44:49 +0100

    #38 | add login logic and error handling

--- firenotes/Views/SignInPage.xaml.cs
+++ firenotes/Views/SignInPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-//using System.Threading.Tasks;
-//using Firebase.Auth;
-//using Firebase.Database;
+using Firebase.Auth;
 using Xamarin.Forms;
 
 namespace firenotes.Views
@@ -30,24 +28,56 @@ namespace firenotes.Views
             string email = txtEmail.Text;
             string password = txtPassword.Text;
 
-            //if (string.IsNullOrWhiteSpace(email))
-            //{
-            //    DisplayError("Sorry, the email field cannot be empty.");
-            //    return;
-            //}
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                DisplayError("Sorry, the email field cannot be empty.");
+                return;
+            }
 
-            //if (string.IsNullOrEmpty(password))
-            //{
-            //    DisplayError("Sorry, the password field cannot be empty.");
-            //    return;
-            //}
+            if (string.IsNullOrEmpty(password))
+            {
+                DisplayError("Sorry, the password field cannot be empty.");
+                return;
+            }
 
             this.spnrLoading.IsVisible = true;
             this.btnSignIn.IsEnabled = false;
 
-            // Navigate to the home page
-            Navigation.InsertPageBefore(new HomePage(), this);
-            await Navigation.PopAsync();
+            try
+            {
+                App.AuthLink = await App.AuthProvider.SignInWithEmailAndPasswordAsync(email, password);
+
+                this.spnrLoading.IsVisible = false;
+                this.btnSignIn.IsEnabled = true;
+
+                // Navigate to the home page
+                Navigation.InsertPageBefore(new HomePage(), this);
+                await Navigation.PopAsync();
+            }
+            catch (FirebaseAuthException ex)
+            {
+                if (ex.Reason == AuthErrorReason.UnknownEmailAddress)
+                {
+                    DisplayError("Sorry, no account exists with that email address.");
+                }
+                else if (ex.Reason == AuthErrorReason.InvalidEmailAddress)
+                {
+                    DisplayError("Sorry, the email address provided is invalid");
+                }
+                else if (ex.Reason == AuthErrorReason.WrongPassword)
+                {
+                    DisplayError("Sorry, the password provided is wrong.");
+                }
+                else
+                {
+                    DisplayError("Sorry, an error occurred. Please try again.");
+                }
+
+                this.spnrLoading.IsVisible = false;
+                this.btnSignIn.IsEnabled = true;
+            }
+
+
         }
 
         protected void GoToSignUp()
