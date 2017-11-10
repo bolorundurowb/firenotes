26cf13589e34d02606f5b977d74ef81055da1f17
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-10 19:01:45 +0100

    #50 | add the user model

---
+++ firenotes/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace firenotes.Models
+{
+    public class User
+    {
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
