799c96e28d7f6f16faf0b0155a9644958b815664
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-11 12:34:51 +0100

    #28 | add logic to favorite and unfavorite a note

--- firenotes/Views/NoteDetailsPage.xaml.cs
+++ firenotes/Views/NoteDetailsPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Firebase.Database;
+using Firebase.Database.Query;
 using firenotes.Models;
 using Xamarin.Forms;
 
@@ -6,9 +8,21 @@ namespace firenotes.Views
 {
     public partial class NoteDetailsPage : ContentPage
     {
+        private Note _note;
+        private static string nodeUrl = $"{Constants.FirebaseUrl}users/{App.AuthLink.User.LocalId}";
+        FirebaseClient firebaseClient = new FirebaseClient(
+            nodeUrl,
+            new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = async () => App.AuthLink.FirebaseToken
+            });
+
         public NoteDetailsPage(Note note)
         {
             InitializeComponent();
+
+            // set the note being viewed
+            this._note = note;
 
             // set the page title
             this.Title = "Note Details";
@@ -39,12 +53,34 @@ namespace firenotes.Views
 
         private async void FavouriteNote(object sender, EventArgs e)
         {
-            
+            // set the favourited state
+            _note.IsFavorite = true;
+            _note.Updated = DateTime.Now;
+
+            await firebaseClient
+                .Child("notes")
+                .Child(_note.Id)
+                .PutAsync(_note);
+
+            // hide the favorite button and show the unfavorite toolbar item
+            tlbFavourite.IsVisible = false;
+            tlbUnfavorite.IsVisible = true;
         }
 
         private async void UnFavouriteNote(object sender, EventArgs e)
         {
-            
+            // set the favourited state
+            _note.IsFavorite = false;
+            _note.Updated = DateTime.Now;
+
+            await firebaseClient
+                .Child("notes")
+                .Child(_note.Id)
+                .PutAsync(_note);
+
+            // hide the favorite button and show the unfavorite toolbar item
+            tlbFavourite.IsVisible = true;
+            tlbUnfavorite.IsVisible = false;
         }
     }
 }
